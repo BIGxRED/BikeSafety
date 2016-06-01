9b5ff1b7802d9b66a12c7f88bb47e086b98e1d99
U-DS\QNP684 <QNP684@CCDTLAB-355.ds.mot.com>
2016-06-01 13:11:27 -0500

    Started playing around with ArrayList in the Slide1 class

--- app/src/main/java/com/example/android/bikesafety/Slide1.java
+++ app/src/main/java/com/example/android/bikesafety/Slide1.java
@@ -3,13 +3,47 @@ package com.example.android.bikesafety;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Slide1 extends AppCompatActivity {
+
+    int index = 0;
+
+    String fact1_Header = getString(R.string.fact1_header);
+    String fact1_Body = getString(R.string.fact1_body);
+
+    String fact2_Header = getString(R.string.fact2_header);
+    String fact2_Body = getString(R.string.fact2_body);
+
+    String fact3_Header = getString(R.string.fact3_header);
+    String fact3_Body = getString(R.string.fact3_body);
+
+    ArrayList<String> bodyText = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide1);
         Intent intent = getIntent();
+        setupArrays();
     }
+
+    public void displayFacts(View view){
+        TextView facts_Body = (TextView) findViewById(R.id.factTextField);
+        if (index < bodyText.size()) {
+            facts_Body.setText(bodyText.get(index));
+            index++;
+        }
+    }
+
+    private void setupArrays(){
+        bodyText.add(fact1_Body);
+        bodyText.add(fact2_Body);
+        bodyText.add(fact3_Body);
+    }
+
+
 }
