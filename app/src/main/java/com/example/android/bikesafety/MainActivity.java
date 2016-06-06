6b8648c3490aed102b1693c6c7bde1463bef8fc4
Mike Palarz <mpalarz1@gmail.com>
2016-06-06 00:32:55 -0500

    Adjusted Activity label for both activities; minor aesthetic changes

--- app/src/main/java/com/example/android/bikesafety/MainActivity.java
+++ app/src/main/java/com/example/android/bikesafety/MainActivity.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flag = false;
     }
 
-    public void displayFacts(View view){
-        Intent intent = new Intent(this, Facts.class);
-        startActivity(intent);
+    public void displayText(View view){
+        TextView welcomeText = (TextView) findViewById(R.id.welcomeTextBody);
+        if (flag == false){
+            welcomeText.setText(R.string.introBodyText2);
+            flag = true;
+        }
+        else {
+            Intent intent = new Intent(this, Facts.class);
+            startActivity(intent);
+        }
     }
 }
